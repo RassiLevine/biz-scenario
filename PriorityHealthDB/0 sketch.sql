9cfac12bb33647118480aa5767e12f0cdf4feba2
AvigailGellis <agellis@cpucodeschool.com>
2023-11-26 03:50:40 -0600

    Work complete

---
+++ PriorityHealthDB/0 sketch.sql
@@ -0,0 +1,8 @@
+/*
+Staff
+	FirstName varchar(30) not null not blank
+	LastName varchar(30) not null not blank
+	Shift char(2) not null either am or pm
+	DateHired date not null between january 2010 and getdate()
+	Position char(2) not null must be dr,np or pa
+*/
---
+++ PriorityHealthDB/1 database.sql
@@ -0,0 +1,6 @@
+use master
+go
+drop database if exists PriorityHealthDB
+go
+create database PriorityHealthDB
+go
